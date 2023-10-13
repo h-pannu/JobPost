d37f916921435802d77edc4d662e403578dd5c61
Harpreet Pannu <er.harpreetpannu@live.com>
2023-10-12 22:18:26 -0700

    Setup INotifyPropertyChnaged in both Model and ViewModel for Job.

---
+++ JobPost/Models/Job.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPost.Models
+{
+    public class Job : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public Job()
+		{
+
+		}
+
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; OnPropertyChanged(nameof(Title)); }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; OnPropertyChanged(nameof(Description)); }
+		}
+
+		private string _skillsReq;
+
+		public string SkillsReq
+		{
+			get { return _skillsReq; }
+			set { _skillsReq = value; OnPropertyChanged(nameof(SkillsReq)); }
+		}
+
+		private int _minExp;
+
+		public int MinExp
+		{
+			get { return _minExp; }
+			set { _minExp = value; OnPropertyChanged(nameof(MinExp)); }
+		}
+
+
+	}
+}
---
+++ JobPost/ViewModel/JobListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using JobPost.Models;
+
+namespace JobPost.ViewModel
+{
+    public class JobListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public JobListViewModel()
+        {
+
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Job _job;
+
+        public Job Job
+        {
+            get { return _job; }
+            set { _job = value; OnPropertyChanged(nameof(Job)); }
+        }
+
+
+    }
+}
